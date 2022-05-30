a5eaf210a2a97c741ab6f66e2eb11ccea1408b93
Yaping Xin (Aerospace) <yaping.xin@foxmail.com>
2022-05-30 17:48:42 +0800

    Add main.c

---
+++ Dev/Code/Utilities/HierarchySQLiteUtil/main.c
@@ -0,0 +1,11 @@
+// HierarchySQLiteUtil | main.c : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
